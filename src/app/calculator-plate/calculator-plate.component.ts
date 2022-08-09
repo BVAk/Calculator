28eb0c7e36f65d695216220feb3cf9c8cd58495b
BVAk <bekkviktoria55@gmail.com>
2022-08-09 19:00:07 +0300

    remove unnecessary files, add calculaor plate

---
+++ src/app/calculator-plate/calculator-plate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-plate',
+  templateUrl: './calculator-plate.component.html',
+  styleUrls: ['./calculator-plate.component.css']
+})
+export class CalculatorPlateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
