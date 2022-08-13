7b8d1303820e2920d3efc5a8818f4e64bbd2b742
BVAk <bekkviktoria55@gmail.com>
2022-08-13 22:30:48 +0300

    start form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 import { CalculatorPlateComponent } from './calculator-plate/calculator-plate.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   declarations: [AppComponent, CalculatorPlateComponent],
   bootstrap: [AppComponent],
 })
--- src/app/calculator-plate/calculator-plate.component.ts
+++ src/app/calculator-plate/calculator-plate.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator-plate',
   templateUrl: './calculator-plate.component.html',
-  styleUrls: ['./calculator-plate.component.css']
+  styleUrls: ['./calculator-plate.component.css'],
 })
-export class CalculatorPlateComponent implements OnInit {
+export class CalculatorPlateComponent {
+  checkoutForm = this.formBuilder.group({
+    calculation: '',
+  });
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit(): void {
+    // Process checkout data here
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
-
 }
